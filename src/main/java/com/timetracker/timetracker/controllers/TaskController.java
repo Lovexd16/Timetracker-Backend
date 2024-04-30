976c69ccbdaf3410a2563237b304ab8a0471e85b
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-30 09:19:51 +0200

    Nu tillåts frontenden

--- src/main/java/com/timetracker/timetracker/controllers/TaskController.java
+++ src/main/java/com/timetracker/timetracker/controllers/TaskController.java
@@ -2,6 +2,7 @@ package com.timetracker.timetracker.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.timetracker.timetracker.models.Task;
 import com.timetracker.timetracker.services.TaskService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class TaskController {
 
