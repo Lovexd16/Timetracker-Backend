959da8c37aabf7c1c16cc10536bcff1f67892289
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-30 08:42:39 +0200

    Man kan nu lägga till tasks genom postman, sparas i databas

--- src/main/java/com/timetracker/timetracker/services/TaskService.java
+++ src/main/java/com/timetracker/timetracker/services/TaskService.java
@@ -1,8 +1,19 @@
 package com.timetracker.timetracker.services;
 
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
+
+import com.timetracker.timetracker.models.Task;
 
 @Service
 public class TaskService {
+    private final MongoOperations mongoOperations;
 
+    public TaskService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public Task addTask(Task task) {
+        return mongoOperations.insert(task);
+    }
 }
