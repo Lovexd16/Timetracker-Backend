6c3d1a18a94bd9d009bacede9610d7ddfe37b0a1
Love Rumar Karlquist <lovebandy@hotmail.se>
2024-04-30 08:34:36 +0200

    TaskService skapad
    Samt liten bit på controller. fixes #9

--- src/main/java/com/timetracker/timetracker/controllers/TaskController.java
+++ src/main/java/com/timetracker/timetracker/controllers/TaskController.java
@@ -1,8 +1,21 @@
 package com.timetracker.timetracker.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.timetracker.timetracker.models.Task;
 
 @RestController
 public class TaskController {
 
+    @GetMapping
+    public String getRoot() {
+        return "{'Hello': 'World!'}";
+    }
+
+    @PostMapping("/task")
+    public Task addTask() {
+        return null;
+    }
 }
